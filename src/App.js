439f0d70cf2375d0268cac27dc98a238bb0fe0c2
zdebar <157598603+zdebar@users.noreply.github.com>
2024-07-31 12:29:57 +0200

    lifting state up to Game

--- src/App.js
+++ src/App.js
@@ -11,12 +11,10 @@ function Square({ value, onSquareClick }) {
 }
 
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+function Board({ xIsNext, squares, onPlay }) {
+  
   function handleClick(i) {
-    if (squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     const nextSquares = squares.slice();
@@ -25,8 +23,7 @@ function Board() {
     } else {
       nextSquares[i] = "O";
     }
-    setXIsNext(!xIsNext)
-    setSquares(nextSquares);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -61,10 +58,19 @@ function Board() {
 
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
